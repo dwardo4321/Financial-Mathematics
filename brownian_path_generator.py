791981559139a9a1ae93325e4847d523b29372d8
dwardo4321 <chidatapiwa@gmail.com>
2026-03-03 16:49:38 +0000

    day 2

--- brownian_path_generator.py
+++ brownian_path_generator.py
@@ -3,16 +3,21 @@ import matplotlib.pyplot as plt
 # import scipy.stats as stats
 import pandas as pd
 
-def path_generator(n: int, t: float, sig: float, num_gens: int, plot=False):
+def path_generator(n: int, t: float, sig: float, num_gens: int, plot=False, correlated: tuple[object, np.ndarray] = (False, None)):
     dt = t / (n - 1)
     w_t = []
 
     for j in range(num_gens):
-        norm_rv = np.random.normal(0, sig * np.sqrt(dt), n-1)
-        path = np.concatenate(([0], np.cumsum(norm_rv)))
-        w_t.append(path)
-    out = np.column_stack(w_t)
-    out = pd.DataFrame(out)
+        norm_rv = np.random.normal(0, sig * np.sqrt(dt), (n-1, num_gens))
+        out = pd.DataFrame(np.vstack((np.zeros((1, num_gens)), np.cumsum(norm_rv, axis=0))), columns=np.arange(1, num_gens+1, 1))
+
+    if correlated[0]:
+        l_matrix = np.linalg.cholesky(correlated[1])
+        correlated_out = l_matrix * correlated[1]
+        z = np.random.normal(0, 1, size=(n-1, num_gens))
+        dw = np.sqrt(dt) * (z @ l_matrix.T) * sig
+        out = pd.DataFrame(np.vstack([np.zeros((1,num_gens)), np.cumsum(dw, axis=0)]), columns=np.arange(1, num_gens+1, 1))
+
     if plot:
         plt.figure(figsize = (10, 5), facecolor='darkgrey')
         plt.gca().set_facecolor("pink")
@@ -24,5 +29,19 @@ def path_generator(n: int, t: float, sig: float, num_gens: int, plot=False):
     else:
         return out.head()
 
-print(path_generator(1000, 5, 0.8, 5, plot=True))
+print(path_generator(1000, 5, 0.8, 3, plot=False))  # Ex 1
+
+corr1 = np.array([[1.00, 0.70, 0.20, -0.10, 0.35],
+                 [0.70, 1.00, 0.25,  0.05, 0.40],
+                 [0.20, 0.25, 1.00,  0.60, 0.15],
+                 [-0.10, 0.05, 0.60, 1.00, 0.10],
+                 [0.35, 0.40, 0.15,  0.10, 1.00]])
+
+print(path_generator(1000, 5, 0.8, corr1.shape[0], plot=True, correlated=(True, corr1)))  # Ex 2
+
+dim = 50
+corr2 = np.full((dim, dim), 0.75)
+np.fill_diagonal(corr2, 1)
+
+print(path_generator(1000, 5, 0.8, dim, plot=True, correlated=(True, corr2)))  # Ex 3
 
---
+++ quad_var_cov_estimator.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# import scipy.stats as stats
+import pandas as pd
+
+# 1 --------------------- Quadratic variation for a random walk --------------------------------------------------------
+def qv_random_walk(n: int, loc: float, scale: float, plot=False):
+    x_i = np.random.normal(loc, scale, size=n)
+    m_i = []; qv_i = []
+    m_i.append(x_i[0]); qv_i.append(x_i[0] ** 2)
+    for i in range(len(x_i)):
+        m_i.append(x_i[i] + m_i[i])
+        qv_i.append((x_i[i]**2) + qv_i[i])
+    out = pd.DataFrame({"Random Walk": m_i, "Quadratic Variation": qv_i})
+
+    if plot:
+        fig = plt.figure(figsize=(18, 5), facecolor="darkgrey")
+        y_labels = ["M_k", "Cumulative Value"]
+        titles = ["Symmetric Random Walk", "Quadratic Variation vs Theoretical Variance"]
+
+        for i, data in enumerate(out.columns):
+            sub = fig.add_subplot(1, 2, i+1)
+            plt.gca().set_facecolor("pink")
+            sub.plot(out.index, out[data])
+            if data == "Quadratic Variation":
+                sub.plot(out.index, (scale**2)*out.index, linestyle='--')
+                sub.legend(["Quadratic Variation", "Theoretical Variance"], edgecolor='navy')
+            sub.set_ylabel(y_labels[i], fontsize=10)
+            sub.set_xlabel('k', fontsize=10)
+            sub.set_title(titles[i], fontsize=12)
+            plt.grid()
+            plt.tight_layout()
+        return out.head(), plt.show()
+    else:
+        return out.head()
+
+print(qv_random_walk(1000, 0, 0.1, plot=True))
+
+# 2 --------------------- Quadratic variation of Brownian motion -------------------------------------------------------
+#def qv_brownian_motion(scale1: float, scale2: float):
+# 3 --------------------- Realized volatility from GBM -------------------------------------------------------
+
+# 4 --------------------- Quadratic covariation / cross-variation ------------------------------------------------------
+
