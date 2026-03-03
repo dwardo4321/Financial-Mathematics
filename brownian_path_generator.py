757e4f33a028f35cf8217ff140fdf5b1057a37f1
dwardo4321 <chidatapiwa@gmail.com>
2026-03-03 12:05:46 +0000

    Day 2: Project 1

--- brownian_path_generator.py
+++ brownian_path_generator.py
@@ -1,31 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.stats as stats
+# import scipy.stats as stats
 import pandas as pd
-N = 100000
-norm_rv1 = np.random.normal(0, 2, N-1)
-norm_rv2 = np.random.normal(0, 2, N-1)
 
-#  1 ----------------------------------------------------------
-W_T1 = np.zeros((N, 1)); W_T2 = np.zeros((N, 1))
+def path_generator(n: int, t: float, sig: float, num_gens = 2, plot=False):
+    dt = t / (n - 1)
+    w_t = []
 
-norm_rv = [norm_rv1, norm_rv2]
-wp = [W_T1, W_T2]
+    for j in range(num_gens):
+        norm_rv = np.random.normal(0, sig * np.sqrt(dt), n-1)
+        path = np.concatenate(([0], np.cumsum(norm_rv)))
+        w_t.append(path)
+    out = np.column_stack(w_t)
+    out = pd.DataFrame(out)
+    if plot:
+        plt.figure(figsize = (10, 5), facecolor='darkgrey')
+        plt.gca().set_facecolor("pink")
+        plt.plot(out)
+        plt.xlabel('t', fontsize = 15)
+        plt.ylabel('W(t)', fontsize=15)
+        plt.grid()
+        return out.head(), plt.show()
+    else:
+        return out.head()
 
-for data, w_t in zip(norm_rv, wp):
-    for i, rv in enumerate(data):
-        w_t[i + 1, ] += w_t[i, ] + rv
+print(path_generator(1000, 1, 0.2, 100, plot=True))
 
-#  2 ----------------------------------------------------------
-# Shorter, no loops
-# W_T1 = np.concatenate(([0], np.cumsum(norm_rv1)))[:, None]
-# W_T2 = np.concatenate(([0], np.cumsum(norm_rv2)))[:, None]
-
-phi = 0.95
-W_T3 = phi * W_T1 + np.sqrt(1 - phi**2) * W_T2
-
-plt.figure(figsize = (19, 10))
-plt.plot(W_T1, color = 'b')
-plt.plot(W_T2, color = 'r')
-plt.plot(W_T3, color = 'black')
-plt.show()
--- testing.py
+++ testing.py
@@ -4,8 +4,17 @@ import scipy.stats as stats
 import pandas as pd
 
 N = 100000
-norm_rv1 = np.random.normal(0, 100, N-1)
-norm_rv2 = np.random.normal(0, 100, N-1)
+norm_rv1 = np.random.normal(0, 1.5, N-1)
+norm_rv2 = np.random.normal(0, 1.5, N-1)
 
 W_T1 = np.concatenate(([0], np.cumsum(norm_rv1)))[:, None]
 W_T2 = np.concatenate(([0], np.cumsum(norm_rv2)))[:, None]
+
+phi = 0.95
+W_T3 = phi * W_T1 + np.sqrt(1 - phi**2) * W_T2
+
+plt.figure(figsize = (19, 10))
+plt.plot(W_T1, color = 'b')
+plt.plot(W_T2, color = 'r')
+plt.plot(W_T3, color = 'black')
+plt.show()
