edbf2c0ebd3994ee361eaf413c632eeb28b09084
dwardo4321 <chidatapiwa@gmail.com>
2026-03-04 13:55:48 +0000

    day 3

--- brownian_path_generator.py
+++ brownian_path_generator.py
@@ -3,39 +3,43 @@ import matplotlib.pyplot as plt
 # import scipy.stats as stats
 import pandas as pd
 
-def path_generator(n: int, t: float, sig: float, num_gens: int, plot=False, correlated: tuple[object, np.ndarray] = (False, None)):
+
+def path_generator(n: int, t: float, scale: float, num_gens: int, plot=False, correlated: tuple[object, np.ndarray] = (False, None)):
     dt = t / (n - 1)
     w_t = []
 
     for j in range(num_gens):
-        norm_rv = np.random.normal(0, sig * np.sqrt(dt), (n-1, num_gens))
-        out = pd.DataFrame(np.vstack((np.zeros((1, num_gens)), np.cumsum(norm_rv, axis=0))), columns=np.arange(1, num_gens+1, 1))
+        norm_rv = np.random.normal(0, scale * np.sqrt(dt), (n - 1, num_gens))
+        out = pd.DataFrame(np.vstack((np.zeros((1, num_gens)), np.cumsum(norm_rv, axis=0))),
+                           columns=np.arange(1, num_gens + 1, 1))
 
     if correlated[0]:
         l_matrix = np.linalg.cholesky(correlated[1])
         correlated_out = l_matrix * correlated[1]
-        z = np.random.normal(0, 1, size=(n-1, num_gens))
-        dw = np.sqrt(dt) * (z @ l_matrix.T) * sig
-        out = pd.DataFrame(np.vstack([np.zeros((1,num_gens)), np.cumsum(dw, axis=0)]), columns=np.arange(1, num_gens+1, 1))
+        z = np.random.normal(0, 1, size=(n - 1, num_gens))
+        dw = np.sqrt(dt) * (z @ l_matrix.T) * scale
+        out = pd.DataFrame(np.vstack([np.zeros((1, num_gens)), np.cumsum(dw, axis=0)]),
+                           columns=np.arange(1, num_gens + 1, 1))
 
     if plot:
-        plt.figure(figsize = (10, 5), facecolor='darkgrey')
+        plt.figure(figsize=(10, 5), facecolor='darkgrey')
         plt.gca().set_facecolor("pink")
         plt.plot(out)
-        plt.xlabel('t', fontsize = 15)
+        plt.xlabel('t', fontsize=15)
         plt.ylabel('W(t)', fontsize=15)
         plt.grid()
         return out.head(), plt.show()
     else:
         return out.head()
 
+
 print(path_generator(1000, 5, 0.8, 3, plot=False))  # Ex 1
 
 corr1 = np.array([[1.00, 0.70, 0.20, -0.10, 0.35],
-                 [0.70, 1.00, 0.25,  0.05, 0.40],
-                 [0.20, 0.25, 1.00,  0.60, 0.15],
-                 [-0.10, 0.05, 0.60, 1.00, 0.10],
-                 [0.35, 0.40, 0.15,  0.10, 1.00]])
+                  [0.70, 1.00, 0.25, 0.05, 0.40],
+                  [0.20, 0.25, 1.00, 0.60, 0.15],
+                  [-0.10, 0.05, 0.60, 1.00, 0.10],
+                  [0.35, 0.40, 0.15, 0.10, 1.00]])
 
 print(path_generator(1000, 5, 0.8, corr1.shape[0], plot=True, correlated=(True, corr1)))  # Ex 2
 
@@ -44,4 +48,3 @@ corr2 = np.full((dim, dim), 0.75)
 np.fill_diagonal(corr2, 1)
 
 print(path_generator(1000, 5, 0.8, dim, plot=True, correlated=(True, corr2)))  # Ex 3
-
--- quad_var_cov_estimator.py
+++ quad_var_cov_estimator.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 # import scipy.stats as stats
 import pandas as pd
 
+
 # 1 --------------------- Quadratic variation for a random walk --------------------------------------------------------
 def qv_random_walk(n: int, loc: float, scale: float, plot=False):
     x_i = np.random.normal(loc, scale, size=n)
@@ -10,7 +11,7 @@ def qv_random_walk(n: int, loc: float, scale: float, plot=False):
     m_i.append(x_i[0]); qv_i.append(x_i[0] ** 2)
     for i in range(len(x_i)):
         m_i.append(x_i[i] + m_i[i])
-        qv_i.append((x_i[i]**2) + qv_i[i])
+        qv_i.append((x_i[i] ** 2) + qv_i[i])
     out = pd.DataFrame({"Random Walk": m_i, "Quadratic Variation": qv_i})
 
     if plot:
@@ -19,11 +20,11 @@ def qv_random_walk(n: int, loc: float, scale: float, plot=False):
         titles = ["Symmetric Random Walk", "Quadratic Variation vs Theoretical Variance"]
 
         for i, data in enumerate(out.columns):
-            sub = fig.add_subplot(1, 2, i+1)
+            sub = fig.add_subplot(1, 2, i + 1)
             plt.gca().set_facecolor("pink")
             sub.plot(out.index, out[data])
             if data == "Quadratic Variation":
-                sub.plot(out.index, (scale**2)*out.index, linestyle='--')
+                sub.plot(out.index, (scale ** 2) * out.index, linestyle='--')
                 sub.legend(["Quadratic Variation", "Theoretical Variance"], edgecolor='navy')
             sub.set_ylabel(y_labels[i], fontsize=10)
             sub.set_xlabel('k', fontsize=10)
@@ -34,11 +35,26 @@ def qv_random_walk(n: int, loc: float, scale: float, plot=False):
     else:
         return out.head()
 
-print(qv_random_walk(1000, 0, 0.1, plot=True))
+
+# print(qv_random_walk(1000, 0, 0.1, plot=True))
 
 # 2 --------------------- Quadratic variation of Brownian motion -------------------------------------------------------
-#def qv_brownian_motion(scale1: float, scale2: float):
+def qv_brownian_motion(scale: float, t, n: int, num_gens: int, plot=False):
+    import brownian_path_generator as bpg
+    path = bpg.path_generator(n, t, scale, num_gens, plot=False)
+
+    for j in range(len(path.columns))
+    for i in range(len(path.index)-1):
+
+
+    if plot:
+        plt.figure(figsize=(10, 10))
+        plt.plot(qv_bm.index, qv_bm.values)
+        return qv_bm, plt.show()
+    else:
+        return qv_bm
+
+print(qv_brownian_motion(0.1, 2, 1000, 3))
 # 3 --------------------- Realized volatility from GBM -------------------------------------------------------
 
 # 4 --------------------- Quadratic covariation / cross-variation ------------------------------------------------------
-
