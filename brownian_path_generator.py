0d215cb39e14a8d6e92c42d64de987b0f71e76cd
dwardo4321 <chidatapiwa@gmail.com>
2026-03-05 12:25:29 +0000

    Add Financial-Mathematics code into repo

--- brownian_path_generator.py
+++ brownian_path_generator.py
@@ -28,23 +28,23 @@ def path_generator(n: int, t: float, scale: float, num_gens: int, plot=False, co
         plt.xlabel('t', fontsize=15)
         plt.ylabel('W(t)', fontsize=15)
         plt.grid()
-        return out.head(), plt.show()
+        return out, plt.show()
     else:
-        return out.head()
+        return out
 
 
-print(path_generator(1000, 5, 0.8, 3, plot=False))  # Ex 1
+#print(path_generator(1000, 5, 0.8, 3, plot=False))  # Ex 1
 
-corr1 = np.array([[1.00, 0.70, 0.20, -0.10, 0.35],
-                  [0.70, 1.00, 0.25, 0.05, 0.40],
-                  [0.20, 0.25, 1.00, 0.60, 0.15],
-                  [-0.10, 0.05, 0.60, 1.00, 0.10],
-                  [0.35, 0.40, 0.15, 0.10, 1.00]])
+#corr1 = np.array([[1.00, 0.70, 0.20, -0.10, 0.35],
+#                  [0.70, 1.00, 0.25, 0.05, 0.40],
+#                  [0.20, 0.25, 1.00, 0.60, 0.15],
+#                  [-0.10, 0.05, 0.60, 1.00, 0.10],
+#                  [0.35, 0.40, 0.15, 0.10, 1.00]])
 
-print(path_generator(1000, 5, 0.8, corr1.shape[0], plot=True, correlated=(True, corr1)))  # Ex 2
+#print(path_generator(1000, 5, 0.8, corr1.shape[0], plot=True, correlated=(True, corr1)))  # Ex 2
 
-dim = 50
-corr2 = np.full((dim, dim), 0.75)
-np.fill_diagonal(corr2, 1)
+# dim = 50
+# corr2 = np.full((dim, dim), 0.75)
+# np.fill_diagonal(corr2, 1)
 
-print(path_generator(1000, 5, 0.8, dim, plot=True, correlated=(True, corr2)))  # Ex 3
+# print(path_generator(1000, 5, 0.8, dim, plot=True, correlated=(True, corr2)))  # Ex 3
--- quad_var_cov_estimator.py
+++ quad_var_cov_estimator.py
@@ -13,7 +13,7 @@ def qv_random_walk(n: int, loc: float, scale: float, plot=False):
         m_i.append(x_i[i] + m_i[i])
         qv_i.append((x_i[i] ** 2) + qv_i[i])
     out = pd.DataFrame({"Random Walk": m_i, "Quadratic Variation": qv_i})
-
+    # ------------------------------------------------
     if plot:
         fig = plt.figure(figsize=(18, 5), facecolor="darkgrey")
         y_labels = ["M_k", "Cumulative Value"]
@@ -39,22 +39,45 @@ def qv_random_walk(n: int, loc: float, scale: float, plot=False):
 # print(qv_random_walk(1000, 0, 0.1, plot=True))
 
 # 2 --------------------- Quadratic variation of Brownian motion -------------------------------------------------------
-def qv_brownian_motion(scale: float, t, n: int, num_gens: int, plot=False):
+def qv_brownian_motion(scale: float, t, n: int, num_gens: int, plot=False, correlated: tuple[bool, np.ndarray] = (False, None)):
+    # ------------------------------------------------
     import brownian_path_generator as bpg
-    path = bpg.path_generator(n, t, scale, num_gens, plot=False)
-
-    for j in range(len(path.columns))
-    for i in range(len(path.index)-1):
-
-
+    if correlated[0]:
+        path = bpg.path_generator(n, t, scale, num_gens, plot=False, correlated =(True, correlated[1]))
+    else:
+        path = bpg.path_generator(n, t, scale, num_gens, plot=False)
+    # ------------------------------------------------
+    qv_bm = np.zeros((n-1, num_gens))
+    for j in range(num_gens):
+        qv_bm[:, j] = np.cumsum((np.diff(path.iloc[:, j]))**2, axis = 0)
+    qv_bm = pd.DataFrame(qv_bm, columns=np.arange(1, num_gens+1, 1))
+    # ------------------------------------------------
     if plot:
-        plt.figure(figsize=(10, 10))
-        plt.plot(qv_bm.index, qv_bm.values)
+        dt = t/(n-1); x = np.arange(0, t, dt)
+        plt.figure(figsize=(10, 5), facecolor='darkgrey')
+        plt.gca().set_facecolor("pink")
+        plt.plot(x, qv_bm.values)
+        plt.xlabel('t', fontsize=15)
+        plt.ylabel('Quadratic Variation', fontsize=15)
+        plt.grid()
         return qv_bm, plt.show()
     else:
         return qv_bm
 
-print(qv_brownian_motion(0.1, 2, 1000, 3))
-# 3 --------------------- Realized volatility from GBM -------------------------------------------------------
+# print(qv_brownian_motion(0.1, 2, 1000, 1, plot=True))
 
+# 3 --------------------- Realized volatility from GBM -----------------------------------------------------------------
+# This one reduces to the QV of a BM
+def geometric_bm(s_0: float, sig: float, t: float, alph: float, n: int):
+    import brownian_path_generator as bpg
+    bms = bpg.path_generator(n, t, sig, 1)
+    price = s_0 * np.exp(sig*bms.iloc[:, 0].to_numpy() + (alph - 0.5*(sig**2))*np.linspace(0, t, n))
+    out = pd.DataFrame({"Time": np.linspace(0, t, n), "Price": price}).set_index("Time")
+    return out
+
+#test = geometric_bm(1000, 0.2, 20, 0.1, 100000)
+#print(test)
+#plt.figure(figsize=(18, 5))
+#plt.plot(test)
+#plt.show()
 # 4 --------------------- Quadratic covariation / cross-variation ------------------------------------------------------
