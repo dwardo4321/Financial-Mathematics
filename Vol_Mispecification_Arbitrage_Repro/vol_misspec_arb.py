3aa5e7fc7d478c9a1364f3dee45fd390419cd1ad
dwardo4321 <150451525+dwardo4321@users.noreply.github.com>
2026-03-19 16:59:29 +0000

    Initialize vol_misspec_arb.py with imports and function
    Added imports and defined vol_misspec_pc_pricer function.

--- Vol_Mispecification_Arbitrage_Repro/vol_misspec_arb.py
+++ Vol_Mispecification_Arbitrage_Repro/vol_misspec_arb.py
@@ -1,1 +1,13 @@
+from Stochastic_Core_Library.quad_var_cov_estimator import geometric_bm
+#from Delta_Hedging_Simulator.bsm_put_call_price import bsm_put_call_pricer
+from Stochastic_Core_Library.brownian_path_generator import path_generator
+import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def vol_misspec_pc_pricer(parameters, sig_real: float, bank_wthdrw: float, replic_error: float, plot: bool=False):
+    s_0 = parameters[0]; strike = parameters[1]; sig = parameters[2]
+    rate = parameters[3]; T = parameters[4]; alpha = parameters[5]
+    n = parameters[6]; risk_neutral_pricing = parameters[7]
 
